56368dd0eb1b8fd9c85bbb254944a4859ed8d1b9
yuk6ra <yuki.imamura.all@gmail.com>
2022-10-07 18:50:35 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 // App.js
 // useEffect と useState 関数を React.js からインポートしています。
-import myEpicNft from "./utils/MyEpicNFT.json";
+import "./styles/App.css";
+
 import { ethers } from "ethers";
 import React, { useEffect, useState } from "react";
-import "./styles/App.css";
+
 import twitterLogo from "./assets/twitter-logo.svg";
-// Constantsを宣言する: constとは値書き換えを禁止した変数を宣言する方法です。
+import myEpicNft from "./utils/MyEpicNFT.json";
+
 const TWITTER_HANDLE = "yuk6ra";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = "";
+const OPENSEA_LINK = "https://testnets.opensea.io/ja/collection/squarenft-iaoqx0vzg7";
 const TOTAL_MINT_COUNT = 50;
+const CONTRACT_ADDRESS = "0x5f700EbA8E5bC0D6a3Fd5290d65Dc3E8093dEd64";
+
 const App = () => {
-  /*
-   * ユーザーのウォレットアドレスを格納するために使用する状態変数を定義します。
-   */
+
   const [currentAccount, setCurrentAccount] = useState("");
-  /*この段階でcurrentAccountの中身は空*/
-  console.log("currentAccount: ", currentAccount);
-  /*
-   * ユーザーが認証可能なウォレットアドレスを持っているか確認します。
-   */
+
+  // setupEventListener 関数を定義します。
+  // MyEpicNFT.sol の中で event が　emit された時に、
+  // 情報を受け取ります。
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+
+        // Event が　emit される際に、コントラクトから送信される情報を受け取っています。
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(
+            `あなたのウォレットに NFT を送信しました。OpenSea に表示されるまで最大で10分かかることがあります。NFT へのリンクはこちらです: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+        });
+        console.log("Setup event listener!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
     if (!ethereum) {
@@ -39,14 +69,14 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+
+      setupEventListener();
     } else {
       console.log("No authorized account found");
     }
   };
 
-  /*
-   * connectWallet メソッドを実装します。
-   */
+  // connectWallet メソッドを実装します。
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
@@ -65,14 +95,14 @@ const App = () => {
        * ウォレットアドレスを currentAccount に紐付けます。
        */
       setCurrentAccount(accounts[0]);
+
+      setupEventListener();
     } catch (error) {
       console.log(error);
     }
   };
 
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS =
-      "0xDd68aCB7Ad032d5d4260478574C7EDB592a81fB1";
     try {
       const { ethereum } = window;
       if (ethereum) {
@@ -84,10 +114,12 @@ const App = () => {
           signer
         );
         console.log("Going to pop wallet now to pay gas...");
+
         let nftTxn = await connectedContract.makeAnEpicNFT();
         console.log("Mining...please wait.");
         await nftTxn.wait();
-  
+
+        console.log(nftTxn)  
         console.log(
           `Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`
         );
@@ -108,28 +140,29 @@ const App = () => {
       Connect to Wallet
     </button>
   );
-  /*
-   * ページがロードされたときに useEffect()内の関数が呼び出されます。
-   */
-  useEffect(() => {
-    checkIfWalletIsConnected();
-  }, []);
+
+  // Mint NFT ボタンをレンダリングするメソッドを定義します。
+  const renderMintUI = () => (
+    <button
+      onClick={askContractToMintNft}
+      className="cta-button connect-wallet-button"
+    >
+      Mint NFT
+    </button>
+  );
+  
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
           <p className="header gradient-text">My NFT Collection</p>
           <p className="sub-text">あなただけの特別な NFT を Mint しよう💫</p>
-          {/*条件付きレンダリングを追加しました
-          // すでに接続されている場合は、
-          // Connect to Walletを表示しないようにします。*/}
-          {currentAccount === "" ? (
-            renderNotConnectedContainer()
-          ) : (
-            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
-              Mint NFT
-            </button>
-          )}
+          {/*条件付きレンダリング。
+          // すでにウォレット接続されている場合は、
+          // Mint NFT を表示する。*/}
+          {currentAccount === ""
+            ? renderNotConnectedContainer()
+            : renderMintUI()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
@@ -144,4 +177,5 @@ const App = () => {
     </div>
   );
 };
+
 export default App;
